57d87f2196b2abafff1a93d4130fc53c462a5a99
Kevin <kevin.garcia@col.vueling.com>
2022-11-09 20:14:16 +0100

    Second commit. Added Interface functions

--- app/src/main/java/com/example/kotlinfoundations/Individual.kt
+++ app/src/main/java/com/example/kotlinfoundations/Individual.kt
@@ -3,22 +3,22 @@ package com.example.kotlinfoundations
 import java.util.*
 
 fun main() {
+    val individual: Individual = Individual(name = "Kevin", age = 29, gender = 'M')
 
 }
 
 class Individual(
-    private var name: String? = "",
-    private var age: Int? = 0,
-    private var passport: String?,
+    private var name: String = "",
+    private var age: Int = 0,
+    private var passport: String,
     private var gender: Char = 'M',
     private var weight: Int = 0,
     private var height: Int = 0,
-) {
-
     private var counter: Int = 0
+): Individualable {
 
-    constructor(name: String, age: Int, gender: String) : this(passport = "")
-    constructor(name: String, age: Int, passport: String, gender: String, weight: Double, height: Double) : this(passport = "")
+    constructor(name: String, age: Int, gender: Char) : this(passport = "")
+    constructor(name: String, age: Int, passport: String, gender: String, weight: Int, height: Int) : this(passport = "")
 
 }
 
--- app/src/main/java/com/example/kotlinfoundations/Individualable.kt
+++ app/src/main/java/com/example/kotlinfoundations/Individualable.kt
@@ -5,35 +5,35 @@ import kotlin.random.Random
 
 interface Individualable {
 
-    fun calculateBMI(weight: Double, height: Double) {
-        var operation = weight / height * height
-
-        //If its below optimal return -1
-        //If its above optimal return 1
-        //If none of this return 0
-
+    fun calculateBMI(weight:Int, height:Double): Int{
+        val bmi: Double = weight.toDouble() / (height * height)
+        return if (bmi < 18.5) {
+            -1
+        } else if (bmi > 25) {
+            1
+        } else {
+            0
+        }
     }
 
     fun isOverAge(age: Int): Boolean {
-        if (age > 18){
-            return true
-        }else if(age < 18){
-            return false
-        }
-        return true
+        return age >= 18
     }
 
-    private fun checkGender() {
+    fun checkGender(gender: Char): Char {
+        return if(gender == 'M' || gender == 'F') 'M'
+        else gender
 
+    }
+
+    fun String.toString(): String {
+        return "Hola"
     }
 
     //Generate a 8-digit number
-    fun generatePassport() {
-        val randomPassport = List(5) { Random.nextInt(0, 9) }
-        println(randomPassport)
+    fun generatePassport(): Int {
+        return (10000000..99999999).random()
     }
-
-    //toString
 
 
 
